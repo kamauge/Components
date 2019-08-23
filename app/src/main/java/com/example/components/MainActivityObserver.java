5268cf67f4ddc44d9bce1072800fe46773d3b10b
kinyunye <kamaugerald2@gmail.com>
2019-08-23 14:58:31 +0300

    Android Components

---
+++ app/src/main/java/com/example/components/MainActivityObserver.java
@@ -0,0 +1,44 @@
+package com.example.components;
+
+import android.util.Log;
+
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.LifecycleObserver;
+import androidx.lifecycle.OnLifecycleEvent;
+
+public class MainActivityObserver implements LifecycleObserver {
+
+    private String TAG = this.getClass().getSimpleName();
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    public void onCreateEvent(){
+        Log.i(TAG,"Observer ON_CREATE");
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    public void onStartEvent(){
+        Log.i(TAG,"Owner ON_START");
+
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    public void onPauseEvent(){
+        Log.i(TAG,"Owner ON_PAUSE");
+
+    }
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    public void onResumeEvent(){
+        Log.i(TAG,"Owner ON_RESUME");
+
+    }
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    public void onStopEvent(){
+        Log.i(TAG,"Owner ON_STOP");
+
+    }
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    public void onDestoryEvent(){
+        Log.i(TAG,"Owner ON_DESTROY");
+
+    }
+}
